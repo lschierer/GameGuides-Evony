8a812ae98aecfe54bc6c353689e4e2404cba05d7
Luke Schierer <luke@schierer.org>
2023-04-14 01:03:56 -0400

    a super minimal site map

---
+++ app/sitemap.tsx
@@ -0,0 +1,10 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.evonytkrtips.net',
+      lastModified: new Date(),
+    },
+  ];
+}
